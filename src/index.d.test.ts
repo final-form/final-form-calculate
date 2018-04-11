83e4eb8b488d599be6ff2b8538f33d3cffb033b4
Carlos Bonetti <carlosb_bc@hotmail.com>
2018-04-11 14:01:58 -0300

    Add typescript typings

---
+++ src/index.d.test.ts
@@ -0,0 +1,28 @@
+import createDecorator from './'
+
+createDecorator(
+  {
+    field: 'foo',
+    updates: {
+      bar: (value, allValues) => {
+        return value
+      },
+      baz: (value, allValues) => {
+        return value
+      },
+    },
+  },
+  {
+    field: ['bar', 'baz'],
+    updates: (value, field, allValues) => {
+      return { ...allValues }
+    },
+  },
+  {
+    field: /ba/,
+    isEqual: (a, b) => a === b,
+    updates: (value, field, allValues) => {
+      return { ...allValues }
+    },
+  }
+)
---
+++ src/index.d.ts
@@ -0,0 +1,27 @@
+import { Decorator } from 'final-form'
+
+export type FieldName = string
+
+export type FieldPattern = FieldName | RegExp | FieldName[]
+
+export type UpdatesByName = {
+  [FieldName: string]: (value: any, allValues?: Object) => any
+}
+
+export type UpdatesForAll = (
+  value: any,
+  field: string,
+  allValues?: Object,
+) => { [FieldName: string]: any }
+
+export type Updates = UpdatesByName | UpdatesForAll
+
+export type Calculation = {
+  field: FieldPattern,
+  updates: Updates,
+  isEqual?: (a: any, b: any) => boolean,
+}
+
+export default function createDecorator(
+  ...calculations: Calculation[]
+): Decorator
