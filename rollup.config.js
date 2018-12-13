a6ebb9e30379706169ff35ef51cf1b381653830c
Erik Rasmussen <rasmussenerik@gmail.com>
2018-12-13 15:06:32 +0100

    Upgraded deps and babel

--- rollup.config.js
+++ rollup.config.js
@@ -50,10 +50,37 @@ export default {
     babel({
       exclude: 'node_modules/**',
       babelrc: false,
-      presets: [['env', { modules: false }], 'stage-2'],
-      plugins: ['external-helpers']
+      runtimeHelpers: true,
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            modules: false,
+            loose: true
+          }
+        ],
+        '@babel/preset-flow'
+      ],
+      plugins: [
+        ['@babel/plugin-transform-runtime', { useESModules: !cjs }],
+        '@babel/plugin-transform-flow-strip-types',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-syntax-import-meta',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-json-strings',
+        [
+          '@babel/plugin-proposal-decorators',
+          {
+            legacy: true
+          }
+        ],
+        '@babel/plugin-proposal-function-sent',
+        '@babel/plugin-proposal-export-namespace-from',
+        '@babel/plugin-proposal-numeric-separator',
+        '@babel/plugin-proposal-throw-expressions'
+      ]
     }),
-    umd
+    umd || es
       ? replace({
           'process.env.NODE_ENV': JSON.stringify(
             minify ? 'production' : 'development'
