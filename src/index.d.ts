8208d7917e8a4a423ec35413e32c6b5980767344
Wesley <flynn.wes@gmail.com>
2020-12-14 19:23:29 -0500

    added updateOnPristine option

--- src/index.d.ts
+++ src/index.d.ts
@@ -20,6 +20,7 @@ export type Updates = UpdatesByName | UpdatesForAll
 export type Calculation = {
   field: FieldPattern,
   updates: Updates,
+  updateOnPristine?: boolean,
   isEqual?: (a: any, b: any) => boolean,
 }
 
